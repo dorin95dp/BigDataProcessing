cf804e958d355d1dfe4710bc6c19a72b2298e381
Popa <dorin.popa1995@gmail.com>
2017-03-14 16:26:47 +0200

    Add JavaFx application with Axes, Plot and Main implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,40 @@
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class Main {
+// Java 8 code
+public class Main extends Application {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        launch(args);
     }
+
+    @Override
+    public void start(final Stage stage) {
+        Axes axes = new Axes(
+                400, 300,
+                -8, 8, 1,
+                -6, 6, 1
+        );
+
+        Plot plot = new Plot(
+                x -> .25 * (x + 4) * (x + 1) * (x - 2),
+                -8, 8, 0.1,
+                axes
+        );
+
+        StackPane layout = new StackPane(
+                plot
+        );
+        layout.setPadding(new Insets(20));
+        layout.setStyle("-fx-background-color: rgb(35, 39, 50);");
+
+        stage.setTitle("y = \u00BC(x+4)(x+1)(x-2)");
+        stage.setScene(new Scene(layout, Color.rgb(35, 39, 50)));
+        stage.show();
+    }
+
 }
