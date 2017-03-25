a1f69a7c1d372fdb8152925703682d932f940c99
Popa <dorin.popa1995@gmail.com>
2017-03-25 19:37:48 +0200

    Add dependencies for Gson class's library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import com.google.gson.Gson;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Gson g = new Gson();
 
         System.out.println("Hello world!");
     }
