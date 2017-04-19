68b832c003e5615157cd56d2fb4f26ea6f61b83b
Popa <dorin.popa1995@gmail.com>
2017-04-19 21:13:28 +0300

    Create PearsonsCorrelation and print matrix in readable table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,18 +10,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String jsonPath = "C:\\Users\\Popa\\IdeaProjects\\BigDataProcessing\\src\\main\\resources\\myJson";
-        Gson parser = new Gson();
+//        String jsonPath = "C:\\Users\\Popa\\IdeaProjects\\BigDataProcessing\\src\\main\\resources\\myJson";
+//        Gson parser = new Gson();
+//
+//        try {
+//            JsonReader json = new JsonReader(new FileReader(jsonPath));
+//
+//            Group group = parser.fromJson(json, Group.class);
+////            System.out.println(group.students.get(0).name);
+//
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Json file reading error: \n" + e.getMessage());
+//        }
 
-        try {
-            JsonReader json = new JsonReader(new FileReader(jsonPath));
+        // PearsonsCorrelation
+        PearsonsCorrelation pearsonsCorr = new PearsonsCorrelation();
+        double[][]indices = {{1, 2, 4, 8, 16},
+                             {2, 4, 8, 16, 32},
+                             {3, 5, 7, 9, 11},
+                             {2, 4, 6, 8, 10}
+        };
 
-            Group group = parser.fromJson(json, Group.class);
-            System.out.println(group.students.get(0).name);
+        RealMatrix realMatrixCorrelations = pearsonsCorr.computeCorrelationMatrix(indices);
+        double[][]correlations = realMatrixCorrelations.getData();
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Json file reading error: \n" + e.getMessage());
-        }
-
+        printCorrelations(correlations);
     }
+
+    static void printCorrelations (double[][] corrs) {
+        System.out.println("The correlation table: \n");
+        
+        for (int rows = 0; rows < corrs.length; rows++){
+            for (int columns = 0; columns < corrs[rows].length; columns++){
+                System.out.print(String.format("%.3f \t", corrs[rows][columns]));
+            }
+            System.out.println();
+        }
+    }
+
+
 }
